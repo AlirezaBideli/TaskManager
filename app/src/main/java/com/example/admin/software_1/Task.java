6d7aa9ffee70acec39fce8fcebef7a45eef2c4dd
Alirezabideli <alirezabideli97@gmail.com>
2018-12-23 23:07:48 +0330

    Add Task Class for user tasks

---
+++ app/src/main/java/com/example/admin/software_1/Task.java
@@ -0,0 +1,51 @@
+package com.example.admin.software_1;
+
+import java.util.Date;
+
+/**
+ * Created by ADMIN on 12/23/2018.
+ */
+
+public class Task {
+
+    private String mTitle;
+    private String mDescription;
+    private Date mDate;
+    private Date mHour;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Date getHour() {
+        return mHour;
+    }
+
+    public void setHour(Date hour) {
+        mHour = hour;
+    }
+
+
+
+
+}
